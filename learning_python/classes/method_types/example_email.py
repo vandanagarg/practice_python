0ea075a0b818c0626298ed5a2b940d24407e53a3
vandana garg <er.vandanagarg@gmail.com>
2020-03-26 18:14:45 +0100

    added example for class variables in python

---
+++ learning_python/classes/method_types/example_email.py
@@ -0,0 +1,19 @@
+# Two type of variables:
+# Instance variables
+# Class variables
+
+
+class User:
+
+    counter = 0
+
+    @classmethod
+    def send_email(cls, email):
+        User.counter += 1
+        print(email)
+
+
+User.send_email("er.vandana")
+User.send_email("peyush")
+User.send_email("singla")
+print(User.counter)
