2cb21fd40868463d401a96f758781387232faac1
vandana garg <er.vandanagarg@gmail.com>
2020-03-27 17:00:27 +0100

    fixed errors in the code by passing valid arguments

--- learning_python/classes/method_types/example_class_pizza.py
+++ learning_python/classes/method_types/example_class_pizza.py
@@ -1,10 +1,12 @@
 class Pizza:
-    def __init__(self, ingredients, sauce):  # constructor
+    def __init__(self, ingredients, sauce, money):  # constructor
         self.ingredients = ingredients
         self.sauce = sauce
+        self.money = money
 
     def __repr__(self):
-        return f'Pizza with {self.ingredients} , {self.sauce} '
+        return f'Pizza with {self.ingredients} , {self.sauce} \
+        costing {self.money}'
 
     @classmethod
     def margherita(cls, sauce):
@@ -13,14 +15,14 @@ class Pizza:
 
     @classmethod
     def prosciutto(cls):
-        return cls(["Cheese", "Tomatoes", "ham", "mushrooms"])
+        return cls(["Cheese", "Tomatoes", "ham", "mushrooms"], "", "")
 
 
-# print(Pizza(["Cheese", "Tomatoes", "olive"]))
+print(Pizza(["Cheese", "Tomatoes", "olive"], "", ""))
 # Pizza with (['Cheese', 'Tomatoes', 'olive'])
 
 print(Pizza.margherita("tomato"))
 # Pizza with (['Cheese', 'Tomatoes'])
 
-# print(Pizza.prosciutto("mango"))
+print(Pizza.prosciutto())
 # Pizza with (['Cheese', 'Tomatoes', 'ham', 'mushrooms'])
