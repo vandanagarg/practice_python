32736f4446397be917b0f088ea65b87db3a0a4f8
vandana garg <er.vandanagarg@gmail.com>
2020-03-27 16:53:06 +0100

    added example for __repr__ inbuilt function

--- learning_python/classes/method_types/instance_variables_python.py
+++ learning_python/classes/method_types/instance_variables_python.py
@@ -7,32 +7,47 @@ class User:
 
     counter = 0  # class variable
 
-    def __init__(self, email):
+    def __init__(self, name, email):
+        self.name = name  # instance variable
         self.email = email  # instance variable
 
     def send_email(self):
-        User.counter += 1
-        print(self.email)
+        User.counter += 3
+        return self.email
+
+    def show_repr(self):
+        return self
 
     def __repr__(self):
-        User.counter += 2
-        return (self.email)
+        User.counter += 1
+        return f"Name is {self.name} and email id is {self.email}."
 
     @classmethod
     def print_count(cls):
         print(User.counter)
 
 
-User("er.vandana").send_email()
-User("peyush").send_email()
-User("singla").send_email()
-User("vsingla").send_email()
+print("\n1. send_email\n")
 
+print(User("vandana", "er.vandana").send_email())
+print(User("p", "peyush").send_email())
+print(User("ps", "singla").send_email())
+print(User("vs", "vsingla").send_email())
+
+print("\n2. show_repr\n")
+
+print(User("vandana", "er.vandana").show_repr())
+print(User("p", "peyush").show_repr())
+print(User("ps", "singla").show_repr())
+print(User("vs", "vsingla").show_repr())
+
+print("\n3. __repr__\n")
 # using __repr__ built in method to return and print output
-print(User("er.vandana"))
-print(User("peyush"))
-print(User("singla"))
-print(User("vsingla"))
+print(User("vandana", "er.vandana"))
+print(User("p", "peyush"))
+print(User("ps", "singla"))
+print(User("vs", "vsingla"))
 
+print("\n4. Total count")
 print(User.counter)
 User.print_count()
