8ae9c6f92516591941388acb51da50ce56f2da82
vandana garg <er.vandanagarg@gmail.com>
2020-05-03 21:50:52 +0200

    added new data structure for offers

--- learning_python/code_kata/kata_0.py
+++ learning_python/code_kata/kata_0.py
@@ -5,13 +5,13 @@ The price is decided on the number of units of product you purchase.
 class PriceCalculator():
     
     DICT_ITEM_COST = {
-        "milk": 59,
-        "apple": 310,
-        "curd": 181, 
-        "cream": 281,
-        "biscuit": 200,
-        "orange": 600,
-        "humus": 300
+        "milk": {"price": 59, "offer": {"quantity": 3, "price": 150}},
+        "curd": {"price": 181}, 
+        "apple": {"price": 310},
+        "cream": {"price": 281, "offer": {"quantity": 4, "price": 700}},
+        "biscuit": {"price": 200},
+        "orange": {"price": 600},
+        "humus": {"price": 300}
     }
 
     def __init__(self, purchase_items_dict):
@@ -19,8 +19,8 @@ class PriceCalculator():
 
     def cal_sp(self):
         selling_price = 0
-        for (item_name,quantity) in purchase_items_dict.items():
-            price = PriceCalculator.DICT_ITEM_COST[item_name] * quantity
+        for (item_name, quantity) in purchase_items_dict.items():
+            price = PriceCalculator.DICT_ITEM_COST[item_name]["price"] * quantity
             selling_price = selling_price + price
         return selling_price
 
@@ -28,3 +28,5 @@ class PriceCalculator():
 purchase_items_dict = {"orange": 2, "humus": 1}
 calculator = PriceCalculator(purchase_items_dict)
 print(calculator.cal_sp())
+
+# offer_dict = {"milk": {"quantity": 3, "price": 150}, "cream": {"quantity": 4, "price": 700}}
