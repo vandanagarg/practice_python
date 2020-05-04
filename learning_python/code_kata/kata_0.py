90f308a7e7387788652ca0d2bbeacd73cfc43ad8
vandana garg <er.vandanagarg@gmail.com>
2020-05-04 13:46:34 +0200

    added code to check offers on products and calculate price

--- learning_python/code_kata/kata_0.py
+++ learning_python/code_kata/kata_0.py
@@ -19,14 +19,32 @@ class PriceCalculator():
 
     def cal_sp(self):
         selling_price = 0
-        for (item_name, quantity) in purchase_items_dict.items():
-            price = PriceCalculator.DICT_ITEM_COST[item_name]["price"] * quantity
+        for (item_name, order_quantity) in purchase_items_dict.items():
+            try:
+                if (PriceCalculator.DICT_ITEM_COST[item_name]["offer"]):
+                    if order_quantity < PriceCalculator.DICT_ITEM_COST[
+                                        item_name]["offer"]["quantity"]:
+                        price = PriceCalculator.DICT_ITEM_COST[item_name
+                                ]["price"] * order_quantity
+                    else:
+                        offer_applied = order_quantity//PriceCalculator.DICT_ITEM_COST[
+                                                        item_name]["offer"]["quantity"]
+                        extra = order_quantity%PriceCalculator.DICT_ITEM_COST[item_name
+                                                                ]["offer"]["quantity"]
+                        price = PriceCalculator.DICT_ITEM_COST[
+                                item_name]["offer"]["price"] * offer_applied + \
+                                PriceCalculator.DICT_ITEM_COST[item_name]["price"] * extra
+            except KeyError:
+                price = PriceCalculator.DICT_ITEM_COST[item_name]["price"] * order_quantity
             selling_price = selling_price + price
         return selling_price
 
 
-purchase_items_dict = {"orange": 2, "humus": 1}
+# purchase_items_dict = {"orange": 2,  "milk": 5, "humus": 1}
+# purchase_items_dict = {"orange": 2, "humus": 1}
+# purchase_items_dict = {"milk": 1}
+# purchase_items_dict = {"milk": 3}
+purchase_items_dict = {"milk": 5, "cream": 2, "orange": 2, "humus": 1}
+# purchase_items_dict = {"cream": 4, "milk": 3}
 calculator = PriceCalculator(purchase_items_dict)
 print(calculator.cal_sp())
-
-# offer_dict = {"milk": {"quantity": 3, "price": 150}, "cream": {"quantity": 4, "price": 700}}
