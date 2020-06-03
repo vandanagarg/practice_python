2d4a4785e1bcf12d7c75ab6c739ab48e4f471d27
vandana garg <er.vandanagarg@gmail.com>
2020-06-03 17:15:17 +0200

    added example for OOPS concept in programming with datatype of type Coordinate

---
+++ learning_python/mit_lectures/functions/Coordinate.py
@@ -0,0 +1,29 @@
+'''  '''
+
+
+class Coordinate(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, other):
+        x_diff_sq = (self.x - other.x) ** 2
+        y_diff_sq = (self.y - other.y) ** 2
+        return (x_diff_sq + y_diff_sq) ** 0.5
+
+    def __str__(self):
+        return "<" + str(self.x) + "," + str(self.y) + ">"
+
+
+c = Coordinate(3, 4)
+print(c)
+# <__main__.Coordinate object at 0x000000000238ABC8> o/p without __str__ method
+
+zero = Coordinate(0, 0)
+print(c.distance(zero))
+
+# or
+print(Coordinate.distance(c, zero))
+
+# to check if an object is a Coordinate
+print(isinstance(c, Coordinate))  # True as c is an object of type Coordinate
