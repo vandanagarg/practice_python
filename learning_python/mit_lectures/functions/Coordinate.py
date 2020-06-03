403881c2b23155f7114a254335477f49c2494bae
vandana garg <er.vandanagarg@gmail.com>
2020-06-03 17:36:19 +0200

    added detailed information related to __str__ method and datatype of user defined class objects

--- learning_python/mit_lectures/functions/Coordinate.py
+++ learning_python/mit_lectures/functions/Coordinate.py
@@ -13,14 +13,28 @@ class Coordinate(object):
         return (x_diff_sq + y_diff_sq) ** 0.5
 
     def __str__(self):
+        ''' Python calls __str__ method when used with
+        print on the class object. Here we define that a
+        Coordinate object on print must return below string '''
         return "<" + str(self.x) + "," + str(self.y) + ">"
 
 
 c = Coordinate(3, 4)
+# representation of an object
 print(c)
 # <__main__.Coordinate object at 0x000000000238ABC8> o/p without __str__ method
 
+# also it makes sense that Coordinate itself is a class and its datatype is
+# type as datatype of any other datastructure is just type
+print(Coordinate)  # <class '__main__.Coordinate'>
+print(type(Coordinate))  # <class 'type'>
+print(type(int))  # <class 'type'>
+print(type(list))  # <class 'type'>
+print(type(str))  # <class 'type'>
+print(type("int"))  # <class 'str'>
+
 zero = Coordinate(0, 0)
+print(zero)
 print(c.distance(zero))
 
 # or
