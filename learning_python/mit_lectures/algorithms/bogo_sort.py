e4ea29c94a84ea38d454550ad3853a966b08d47e
vandana garg <er.vandanagarg@gmail.com>
2020-06-09 12:29:03 +0200

    added definition for is_sorted() function

--- learning_python/mit_lectures/algorithms/bogo_sort.py
+++ learning_python/mit_lectures/algorithms/bogo_sort.py
@@ -3,10 +3,24 @@ as we can search through all possible permutations '''
 import random
 
 
+def is_sorted(L):
+    if sorted(L) == L:
+        return True
+    else:
+        return False
+
+
 def bogo_sort(L):
     ''' Crisp definition for bogo sort/ permutation sort
         best case: O(n) where n is len(L) and list is sorted
         worst case: O(?): ;is unbounded and could go for ever
         let is_sorted() be a function to check if list is sorted '''
-    while not is_sorted(L):  # noqa: F821
+    while not is_sorted(L):
+        print(L)
         random.shuffle(L)
+    return L
+
+
+L = [3, 2, 1]
+# L = [1, 2, 3]
+print(bogo_sort(L))
