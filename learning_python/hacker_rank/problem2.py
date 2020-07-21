cc2e7f79546582b69f9b33c5931652e7be574081
vandana garg <er.vandanagarg@gmail.com>
2020-07-21 15:25:08 +0200

    added problem statement for swapcase() and reversing string

---
+++ learning_python/hacker_rank/problem2.py
@@ -0,0 +1,17 @@
+# swapcase and reversing the string
+
+
+def reverse_words_order_and_swap_cases(sentence):
+    return
+
+
+sentence = "aWESOME is cODING"
+# print(len(sentence))
+s = sentence.split()
+print(s)
+s.reverse()
+print(s)
+text = " "
+new = text.join(s)
+print(new)
+print(new.swapcase())
