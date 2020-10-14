aa4fbd20d551d1f1af78c1ef4b2b95d5daa619ec
vandana garg <er.vandanagarg@gmail.com>
2020-10-14 16:48:56 +0200

    added PS for replacing and finding a sub-string within a string

---
+++ learning_python/hacker_rank/problem20.py
@@ -0,0 +1,25 @@
+# replace a string
+
+string = "abracadabra"
+l = list(string)
+l[5] = 'k'
+print(l)
+string = ''.join(l)
+print(string)
+# abrackdabra
+
+# Find a string
+
+string = "ABCDCDCDC"
+sub_string = "CDC"
+
+
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count += 1
+    return count
+
+
+print(count_substring(string, sub_string))
