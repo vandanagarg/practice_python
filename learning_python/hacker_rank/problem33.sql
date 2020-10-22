0dd2781fa0db8875bc9d62ab13d019836e3dcccb
vandana garg <er.vandanagarg@gmail.com>
2020-10-22 12:48:25 +0200

    updated group by condition

--- learning_python/hacker_rank/problem33.sql
+++ learning_python/hacker_rank/problem33.sql
@@ -9,7 +9,8 @@ where c.company_code = l.company_code
     and l.lead_manager_code=s.lead_manager_code 
     and s.senior_manager_code=m.senior_manager_code 
     and m.manager_code=e.manager_code 
-group by c.company_code order by c.company_code;
+group by c.company_code, c.founder
+order by c.company_code;
 
 
 select company_code, founder,
