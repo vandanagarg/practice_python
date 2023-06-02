56bd7ca6596650cb378dc260ed41355658e3bb2c
Vandana Garg <er.vandanagarg@gmail.com>
2023-06-02 11:43:46 +0200

    implementing basic arithmetic examples using type decimal

--- learning_python/data_structures/decimal_type/basic_arithmetic.py
+++ learning_python/data_structures/decimal_type/basic_arithmetic.py
@@ -1,10 +1,10 @@
 from decimal import Decimal
 
 principal = Decimal('1000.00')
-print(principal)
+print("principal: ", principal)
 
 rate = Decimal('0.05')
-print(rate)
+print("rate: ", rate)
 print(type(rate))
 
 x = Decimal('10.5')
@@ -19,4 +19,6 @@ num = 1000.00
 print(num)
 print(f'num is: {num:.20f}')
 
-# for 
+for year in range(1, 11):
+    amount = principal * (1 + rate) ** year
+    print(f'{year:>2}{amount:>10.2f}')
