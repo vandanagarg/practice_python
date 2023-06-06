31526c9cf4e97b3e1fcc111e6c7db9c1a6ec499f
Vandana Garg <er.vandanagarg@gmail.com>
2023-06-06 12:12:19 +0200

    added another example to elaborate enumerate function

--- learning_python/functions/enumerate/enumerate.py
+++ learning_python/functions/enumerate/enumerate.py
@@ -1,5 +1,31 @@
+# enumerate function is used for unpacking sequences, it generates the sequence of tuples
+# each tuple contains 2 elements index and the corresponding element
+
+#  example 1
+
 word = 'abcde'
 
 for item in enumerate(word):
+    print(item) # generating tuple
+    print(type(item))
+
+#  printing only item values at index position 1 
+for item in enumerate(word):
     print(item[1])
 
+
+#  example 2
+
+colors = ['red', 'orange', 'yellow']
+
+print(list(enumerate(colors)))
+
+print(tuple(enumerate(colors)))
+
+
+# Unpacking Sequences
+
+print(f'unpacking the sequence')
+
+for index, value in enumerate(colors):
+    print(f'{index}: {value}')
